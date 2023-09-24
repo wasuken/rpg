dadbe7c45b966abefc5583e657d92c256b7f0e6f
wasuken <wevorence@gmail.com>
2023-09-24 11:40:49 +0900

    パネルをエリア(二次元配列)ではなく都度選択する方式へと修正していく

--- src/main.lisp
+++ src/main.lisp
@@ -12,19 +12,59 @@
 (setf (gethash 'def *player*) 1)
 (setf (gethash 'agi *player*) 1)
 
+;; アイテムごとの増減管理
+(defparameter *approp-table* (make-hash-table))
+(setf (gethash 'hp *approp-table*) '(0 . 10))
+(setf (gethash 'money *approp-table*) '(-1000 . 1000))
+(setf (gethash 'str *approp-table*) '(-10 . 10))
+(setf (gethash 'def *approp-table*) '(-10 . 10))
+(setf (gethash 'agi *approp-table*) '(-10 . 10))
+
+(defparameter *item-table* (make-hash-table))
+(setf (gethash 'red-posion *item-table*) '(amount . 100 description . "HP is healed 100 times."))
+(setf (gethash 'blue-posion *item-table*) '(amount . 100 description . "HP is healed 200 times."))
+(setf (gethash 'sword *item-table*) '(amount . 1000 description . "STR is healed 1 times."))
+(setf (gethash 'shield *item-table*) '(amount . 1000 description . "DEF is healed 1 times."))
+(setf (gethash 'shoes *item-table*) '(amount . 1000 description . "AGI is healed 1 times."))
+
 (defparameter *panel-list*
   '(shop battle status random hp money))
 
-(defun panel-to-str-one (p)
-  (cond ((eq p 'shop) "s")
-	((eq p 'battle) "b")
-	((eq p 'status) "t")
-	((eq p 'random) "r")
-	((eq p 'hp) "h")
-	((eq p 'money) "m")
-	(t "?")
-	)
+(defparameter *panel-event-list*
+  '(shop battle status random hp money))
+
+(defun set-panel-event ()
+  (mapcar #'(lambda (p) `(,p . ,(generate-event p))) *panel-list*))
+
+(defun rand-panel ()
+  (nth (random (length *panel-list*)) *panel-list*))
+
+(defun rise-and-fall (panel)
+  (let ((v-range (gethash panel *approp-table*)))
+    (setf (gethash panel *player*)
+	  (+ (random (- (cdr v-range) (car v-range) -1)) (car v-range)))
+    )
   )
+
+(defun battle ())
+
+(defun shop ()
+  (format t "welcome!~% ~{~A~}"
+	  (mapcar #'(lambda (item) (format nil "" (car item) (cdr item)))))
+  )
+
+(defun generate-event (panel)
+  (cond ((or (eq panel 'status) (eq panel 'hp) (eq panel 'money))
+	 (setf (gethash panel *player*)
+	       (+ (gethash panel *player*) (rise-and-fall p)))
+	 )
+	((eq panel 'shop)
+	 (shop))
+	((eq panel 'battle)
+	 (battle))
+	((eq panel 'random)
+	 (generate-event (rand-panel)))
+	))
 
 (defun interleave-with (lst elem)
   (cdr (reverse
@@ -45,8 +85,7 @@
 
 (defun next-panel-list (n)
   (let ((panel-list '()))
-    (loop for v = (nth (random (length *panel-list*)) *panel-list*)
-	    then (nth (random (length *panel-list*)) *panel-list*)
+    (loop for v = (rand-panel) then (rand-panel)
 	  while (< (length panel-list) n)
 	  do (cond ((eq v 'random)
 		    (push v panel-list))
